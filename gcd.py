39ba93c485011446506605c502d5707a3d90b711
Arseniy <asuzx@yandex.ru>
2019-09-21 18:24:38 +0300

    Updated gcd.py.New feature

--- gcd.py
+++ gcd.py
@@ -1,15 +1,19 @@
-a,b = input().split()
+d1,d2 = input().split()
 
-a=int(a)
-b=int(b)
+a = int(d1)
+b = int(d2)
 
-gcd=a%b
 
-while gcd:
-	a=b
-	b=gcd
-	print("%s %% %s = %s" % (a,b,a%b))
+gcd = a%b
+ 
+
+while True:
 	if a%b == 0:break
+	a = b
+	b = gcd
+	print("%s %% %s = %s" % (a,b,a%b))	
 	gcd = a%b
+	
+lcm=int(d1)*(int(d2)//b)
 
-print(gcd)
+print("gcd:%s\nlcm:%s" %(b,lcm))
